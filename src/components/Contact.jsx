6c9eeda6278b40fba6cdb0919c7abf4179683ae9
blue56789 <yashsawant0903@gmail.com>
2024-06-15 20:26:33 +0530

    gh-pages-2

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,30 @@
 import linkedIn from '../assets/linkedin.svg'
-import instagram from '../assets/instagram.svg'
+import mail from '../assets/mail.svg'
 import github from '../assets/github.svg'
 
 export default function Contact() {
-    return (
-        <div id="contact" className="min-h-screen p-4 flex justify-center items-center">
-            <div className="max-w-5xl">
-                <h1 className="text-7xl font-semibold mb-8">Contact</h1>
-                <div className="flex flex-wrap gap-8 justify-center">
-                    <div className="border-2 bg-purple border-black rounded-lg">
-                        <a href="https://linkedin.com/in/yash-sawant-b0676b271/" target="_blank" className="p-4 flex justify-center items-center hover:bg-hover-shadow transition-all">
-                            <img src={linkedIn} alt="LinkedIn Profile" className="size-16" />
-                        </a>
-                    </div>
-                    <div className="border-2 bg-orange border-black rounded-lg">
-                        <a href="https://instagram.com/yashs903/" target="_blank" className="p-4 flex justify-center items-center hover:bg-hover-shadow transition-all">
-                            <img src={instagram} alt="Instagra Profile" className="size-16" />
-                        </a>
-                    </div>
-                    <div className="border-2 bg-green border-black rounded-lg">
-                        <a href="https://github.com/blue56789/" target="_blank" className="p-4 flex justify-center items-center hover:bg-hover-shadow transition-all">
-                            <img src={github} alt="GitHub Profile" className="size-16" />
-                        </a>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div id="contact" className="min-h-screen p-4 flex justify-center items-center">
+      <div className="max-w-5xl">
+        <h1 className="text-7xl font-semibold mb-8">Contact</h1>
+        <div className="flex flex-wrap gap-8 justify-center">
+          <div className="border-2 bg-blue border-black rounded-lg">
+            <a href="https://linkedin.com/in/yash-sawant-b0676b271/" target="_blank" className="p-4 flex justify-center items-center hover:bg-hover-shadow transition-all">
+              <img src={linkedIn} alt="LinkedIn Profile" className="size-16" />
+            </a>
+          </div>
+          <div className="border-2 bg-purple border-black rounded-lg">
+            <a href="mailto:yashsawantw@gmail.com" target="_blank" className="p-4 flex justify-center items-center hover:bg-hover-shadow transition-all">
+              <img src={mail} alt="Instagra Profile" className="size-16" />
+            </a>
+          </div>
+          <div className="border-2 bg-green border-black rounded-lg">
+            <a href="https://github.com/blue56789/" target="_blank" className="p-4 flex justify-center items-center hover:bg-hover-shadow transition-all">
+              <img src={github} alt="GitHub Profile" className="size-16" />
+            </a>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,29 +1,26 @@
 import { projects } from "../assets/projects";
 export default function Projects() {
-    return (
-        <div id="projects" className="min-h-screen p-4 pt-16 flex justify-center items-center">
-            <div className="max-w-5xl flex flex-col items-center">
-                <h2 className="text-7xl font-semibold">Projects</h2>
-                <div className="flex flex-wrap mt-8 w-full justify-center">
-                    {
-                        projects.map((p, i) =>
-                            <div key={i} className="flex flex-col w-80 border border-black bg-white shadow-lg rounded-lg m-2 p-4">
-                                <img src={p.img} alt={`${p.name} screenshot`} className="w-full rounded-md border border-black" />
-                                <h3 className="text-xl font-semibold py-4 border-b border-border-pr">{p.name}</h3>
-                                <p className="flex-grow text-ellipsis py-4">{p.description}</p>
-                                <div className="pt-4 flex justify-between border-t border-border-pr">
-                                    <div className="bg-yellow rounded-full overflow-hidden border border-black">
-                                        <a href={p.link} target="_blank" className="flex justify-center items-center w-24 border border-border-pr py-2 hover:bg-hover-shadow transition-all font-semibold">Link</a>
-                                    </div>
-                                    <div className="bg-blue rounded-full overflow-hidden border border-black">
-                                        <a href={p.github} target="_blank" className="flex justify-center items-center w-24 border border-border-pr py-2 hover:bg-hover-shadow transition-all font-semibold">GitHub</a>
-                                    </div>
-                                </div>
-                            </div>
-                        )
-                    }
+  return (
+    <div id="projects" className="min-h-screen p-4 pt-16 flex justify-center items-center">
+      <div className="max-w-5xl flex flex-col items-center">
+        <h2 className="text-7xl font-semibold">Projects</h2>
+        <div className="flex flex-wrap mt-8 w-full justify-center">
+          {
+            projects.map((p, i) =>
+              <div key={i} className="flex flex-col w-80 border border-black bg-white shadow-lg rounded-lg m-2 p-4">
+                <img src={p.img} alt={`${p.name} screenshot`} className="w-full rounded-md border border-black" />
+                <h3 className="text-xl font-semibold py-4 border-b border-border-pr">{p.name}</h3>
+                <p className="flex-grow text-ellipsis py-4">{p.description}</p>
+                <div className="pt-4 flex justify-center border-t border-border-pr">
+                  <div className="bg-green rounded-full overflow-hidden border border-black">
+                    <a href={p.github} target="_blank" className="flex justify-center items-center border border-border-pr px-4 py-2 hover:bg-hover-shadow transition-all font-semibold">GitHub</a>
+                  </div>
                 </div>
-            </div>
+              </div>
+            )
+          }
         </div>
-    )
+      </div>
+    </div>
+  )
 }
